fe516b31e8d9c5e172557a0de0fc242ede3bbc3a
Bo Nicolajsen <>
2024-04-23 14:09:26 +0200

    solution

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,38 @@
 // write cool JS hwere!!
+
+// find dom elementer
+let myAmmountElement = document.getElementById("ammount")
+let myExchangeRateElement = document.getElementById("rate")
+let myResultElement = document.getElementById("result")
+let myCalculateButton = document.getElementById("calculateButton")
+
+
+myCalculateButton.addEventListener('click', () => {
+    Calculateresult()
+})
+
+
+function Calculateresult() {
+    let myAmmount = myAmmountElement.value
+    let myRate = myExchangeRateElement.value
+
+    // currency name optional
+    console.log(myExchangeRateElement.options);
+    let myCurrency = myExchangeRateElement.options[myExchangeRateElement.selectedIndex].innerText
+
+
+
+    let result = myAmmount / myRate
+
+    // to fixed er for at begrænse decimaler
+    // to fixed giver færre decimaler (anttal)
+    showResult(result.toFixed(2), myCurrency)
+}
+
+
+
+
+function showResult(result, myCurrency) {
+
+    myResultElement.innerText = result + " " + myCurrency
+}
